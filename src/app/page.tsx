db3bc080816fd29047a53721039786419d2ea5ba
25lp3625 User <25lp3625@administrators-iMac.local>
2026-02-03 13:10:26 +0800

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,100 @@
-import Image from "next/image";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import * as React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Film, Moon, Search } from "lucide-react";
+import { Card, CardContent } from "@/components/ui/card";
+import Autoplay from "embla-carousel-autoplay";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <header className="h-15 w-screen justify-between items-center flex px-5">
+        <div className="flex gap-2">
+          <Film />
+          <h1>Batflix</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="flex gap-3">
+          <Button variant="outline" size="icon" aria-label="Submit">
+            <Search />
+          </Button>
+          <Button variant="outline" size="icon" aria-label="Submit">
+            <Moon />
+          </Button>
         </div>
-      </main>
+      </header>
+      <NowPlaying />
+      <section>
+        <div>
+          <h1>Upcoming</h1>
+          <p>See more</p>
+        </div>
+        <div className="grid grid-cols-2 gap-5">
+          <div className="w-10">helloowww</div>
+          <div className="w-10">helloowww</div>
+          <div className="w-10">helloowww</div>
+          <div className="w-10">helloowww</div>
+          <div className="w-10">helloowww</div>
+        </div>
+      </section>
     </div>
   );
 }
+
+const movieArr = [
+  {
+    img: "/jangum.jpg",
+    id: 1,
+    rating: 6.5,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, excepturi ipsa? Recusandae earum ipsum omnis, modi quidem consequatur rerum ut quaerat dolore, voluptatum ea consequuntur tenetur quo fuga! Omnis, culpa.",
+    name: "Jangum-sama",
+  },
+  {
+    img: "/jangum.jpg",
+    id: 2,
+    rating: 6.5,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, excepturi ipsa? Recusandae earum ipsum omnis, modi quidem consequatur rerum ut quaerat dolore, voluptatum ea consequuntur tenetur quo fuga! Omnis, culpa.",
+    name: "Jangum-sama",
+  },
+  {
+    img: "/jangum.jpg",
+    id: 3,
+    rating: 6.5,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, excepturi ipsa? Recusandae earum ipsum omnis, modi quidem consequatur rerum ut quaerat dolore, voluptatum ea consequuntur tenetur quo fuga! Omnis, culpa.",
+    name: "Jangum-sama",
+  },
+];
+
+const NowPlaying = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 2000, stopOnInteraction: true }),
+  );
+  return (
+    <Carousel
+      plugins={[plugin.current]}
+      className="w-full relative px-3"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {movieArr.map((movie) => (
+          <CarouselItem key={movie.id}>
+            <div className="relative">
+              <img src={movie.img} alt={movie.name} className="" />
+              <h1>{movie.name}</h1>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
